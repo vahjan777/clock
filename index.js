80fc1f03263c5b9897b5651d26cfd377b66195b2
vahjan777 <vahjan777@gmail.com>
2020-08-18 22:04:25 +0400

    poxvac

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
+const time = document.getElementById('time');
+
+
+
 function showTime() {
-    const alarmClock = document.getElementById('alarmClock');
-    let alCl = '<div class="box">';
-
-    let clock = `<div class = 'container'>`;
-    let clockContainer = `<div> <h3 id = "time"> </h3>  </div>`;
-    let timeMenu = `<div> <p>Start Time</p> <input  id = "inputTime" type = "time"><button id = "setButton" onClick = "timeSet()"> Start </button> </div>`;
-    clock = clock + clockContainer + timeMenu + `</div>`;
-    
-    let alarm = `<div class = 'container'>`;
-    let alarmMenu = `<p>Name</p><input><p>time</p><input><button>Create</button>`
-    alarm = alarm + alarmMenu + "</div>";
-
-    alCl = alCl + clock + alarm + '</div>';
-    alarmClock.innerHTML = alCl;
-
-    const time = document.getElementById('time');
     setInterval(() => {
         let date = new Date();
         let hours = date.getHours();
@@ -28,12 +16,13 @@ function showTime() {
         return (time < 10) ? "0" + time : time;
     }
 }
-function timeSet() {
-
-    const value = document.getElementById('inputTime').value;
-    const valueSplit = value.split(':');
-    
-    console.log(valueSplit);
-}
 
 showTime()
+
+function setTime() {
+
+    const value = document.getElementById('inputTime').value;
+    const valueSplited = value.split(':');
+
+
+}
