0f76dd056498a3aa778fbf33b42e6b0600b99d05
vahjan777 <vahjan777@gmail.com>
2020-08-19 11:28:06 +0400

    verj

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const alarmName = document.getElementById('alarmName');
 const alarmTime = document.getElementById('alarmTime');
-const alarmButton = document.getElementById('alarmButton');
 let h,m,s,ah,am,as,animate;
 let GlobalDate = new Date();
 
@@ -29,9 +28,9 @@ function clock(){
     addZero('min', m);
     addZero('hr', h);
     if (h === ah && m === am && s == as){
-        console.log('alaaarm');
+        alert ('alaaarm');
     }
-    animate=setTimeout(clock,1000);
+    animate = setTimeout(clock,1000);
 
 }
 
@@ -47,7 +46,7 @@ init();
 
 
 function setTime() {
-    if (document.getElementById('inputTime').value.length == 0){
+    if (document.getElementById('inputTime').value.length === 0){
         return ;
     }
     const valueSplitted = document.getElementById('inputTime').value.split(':');
@@ -57,16 +56,12 @@ function setTime() {
 }
 
 function setAlarm() {
-    if (alarmTime.value.length == 0 && alarmName.value.length == 0){
-        return ;
+    if (alarmTime.value.length === 0 && alarmName.value.length === 0){
+        return
     }
-    const alarmValue = alarmName.value;
-    const valueSplitted = alarmTime.value.split(':');
 
+    const valueSplitted = alarmTime.value.split(':');
     ah = +valueSplitted[0];
     am = +valueSplitted[1];
     as = '00';
-
-
-
 }
